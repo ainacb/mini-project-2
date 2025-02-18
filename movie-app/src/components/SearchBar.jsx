6c3e5151485540d3b6f02accc01dc55552947f4d
ainacb <ainacb.93@gmail.com>
2025-02-18 21:28:50 +1100

    search bar component added and finalised.
    search input - for title.
    filter for original language and age rating added.
    sorting option available - release date: asc + desc, highest rated.

---
+++ movie-app/src/components/SearchBar.jsx
@@ -0,0 +1,78 @@
+import { FaSearch } from "react-icons/fa";
+
+const SearchBar = ({
+  search,
+  setSearch,
+  handleSearch,
+  tempFilters,
+  setTempFilters,
+}) => {
+  return (
+    <form onSubmit={handleSearch} className="search-form">
+      {/* Search Input */}
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search movies..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      {/* Language Filter */}
+      <select
+        className="filter-select"
+        value={tempFilters.original_language}
+        onChange={(e) =>
+          setTempFilters({ ...tempFilters, language: e.target.value })
+        }
+      >
+        <option value="">Language</option>
+        <option value="en">English</option>
+        <option value="es">Spanish</option>
+        <option value="fr">French</option>
+        <option value="ja">Japanese</option>
+        <option value="ko">Korean</option>
+      </select>
+
+      {/* Age Rating Filter */}
+      <select
+        className="filter-select"
+        value={tempFilters.ageRating}
+        onChange={(e) =>
+          setTempFilters({ ...tempFilters, ageRating: e.target.value })
+        }
+      >
+        <option value="">Age Rating</option>
+        <option value="G">G</option>
+        <option value="PG">PG</option>
+        <option value="PG-13">PG-13</option>
+        <option value="R">R</option>
+      </select>
+
+      {/* Sorting Option */}
+      <select
+        className="filter-select"
+        value={tempFilters.sortBy}
+        onChange={(e) =>
+          setTempFilters({ ...tempFilters, sortBy: e.target.value })
+        }
+      >
+        <option value="">Sort By</option>
+        <option value="vote_average.desc">Highest Rated</option>
+        <option value="release_date.desc">
+          Release Date (Newest to Oldest)
+        </option>
+        <option value="release_date.asc">
+          Release Date (Oldest to Newest)
+        </option>
+      </select>
+
+      {/* Search Button */}
+      <button type="submit" className="search-button">
+        <FaSearch />
+      </button>
+    </form>
+  );
+};
+
+export default SearchBar;
