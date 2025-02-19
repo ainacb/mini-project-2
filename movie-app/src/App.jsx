bfe52865a1851fe1cac3b1caee1e43d29c1eaa1f
ainacb <ainacb.93@gmail.com>
2025-02-19 18:17:40 +1100

    updated comments on app js

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -6,18 +6,18 @@ import SearchBar from "./components/SearchBar";
 import Pagination from "./components/Pagination";
 
 function App() {
-  const [searchInput, setSearchInput] = useState(""); // User input
-  const [searchQuery, setSearchQuery] = useState(""); // Triggers API request
+  const [searchInput, setSearchInput] = useState(""); // User input in search bar
+  const [searchQuery, setSearchQuery] = useState(""); // Triggers API request when search bar is submitted
   const [filters, setFilters] = useState({
     language: "",
     ageRating: "",
     sortBy: "",
-  });
+  }); // Stores Active filters
 
   const [tempFilters, setTempFilters] = useState(filters); // Temporary filters until submit
+  const [currentPage, setCurrentPage] = useState(1); // Tracks current page for pagination
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const { movies, loading, error } = useMovies(searchQuery, filters);
+  const { movies, loading, error } = useMovies(searchQuery, filters); // Fetch movies based on searchQuery and filters
 
   // Pagination Logic - Show only 12 movies per page
   const moviesPerPage = 12;
@@ -35,17 +35,17 @@ function App() {
   return (
     <div className="container">
       <h1 className="title">Movie Finder</h1>
-
-      <SearchBar
+      <SearchBar // Renders searchBar component, props passed to searchBar
         search={searchInput}
         setSearch={setSearchInput}
         handleSearch={handleSearch}
         tempFilters={tempFilters}
         setTempFilters={setTempFilters}
       />
-
       {loading && <p className="loading">Loading movies...</p>}
+      {/* If loading is true, it displays "Loading movies...". */}
       {error && <p className="error">{error}</p>}
+      {/* If error exists, it displays the error message. */}
 
       <div className="movie-grid">
         {currentMovies.length > 0 ? (
@@ -56,7 +56,6 @@ function App() {
           <p className="no-movies">No movies found.</p>
         )}
       </div>
-
       {/* Pagination Component */}
       <Pagination
         currentPage={currentPage}
