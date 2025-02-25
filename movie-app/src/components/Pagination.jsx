a2d8d0a210d1655c78f6ff269f9bd144827ef962
ainacb <ainacb.93@gmail.com>
2025-02-25 17:48:54 +1100

    pagination comments updated

--- movie-app/src/components/Pagination.jsx
+++ movie-app/src/components/Pagination.jsx
@@ -1,23 +1,23 @@
 const Pagination = ({
-  currentPage,
-  totalMovies,
-  moviesPerPage,
-  onPageChange,
+  currentPage, //currently active page number
+  totalMovies, // total number of movies available
+  moviesPerPage, // number of movies displayed per page
+  onPageChange, // function that updates the currentPage when next or previous is clicked
 }) => {
-  const indexOfLastMovie = currentPage * moviesPerPage;
+  const indexOfLastMovie = currentPage * moviesPerPage; // calculates the index of the last movie on the current page, helps determine if there are more movies left to display.
 
   return (
     <div className="pagination">
       <button
         onClick={() => onPageChange(currentPage - 1)}
-        disabled={currentPage === 1}
+        disabled={currentPage === 1} // prevents navigation if the user is already on page 1.
       >
         ◀ Previous
       </button>
       <span> Page {currentPage} </span>
       <button
         onClick={() => onPageChange(currentPage + 1)}
-        disabled={indexOfLastMovie >= totalMovies}
+        disabled={indexOfLastMovie >= totalMovies} //  prevents navigation if there are no more movies left.
       >
         Next ▶
       </button>
